acb29c70497b6c2ac445302f140be348b877f6af
Yevhenii Sen <jeximst@gmail.com>
2019-11-07 22:30:54 +0200

    Updates

---
+++ precache-manifest.547a7f591d1692f5555379b771ed8a28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad15d1716b6ffa1cd069b0e80fb4005",
+    "url": "/warehouses-ui-test/index.html"
+  },
+  {
+    "revision": "a45ade8164cf594af996",
+    "url": "/warehouses-ui-test/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "1d1d2c9efe942d378e09",
+    "url": "/warehouses-ui-test/static/css/main.6f060f28.chunk.css"
+  },
+  {
+    "revision": "a45ade8164cf594af996",
+    "url": "/warehouses-ui-test/static/js/2.70396ff9.chunk.js"
+  },
+  {
+    "revision": "1d1d2c9efe942d378e09",
+    "url": "/warehouses-ui-test/static/js/main.b65cc53d.chunk.js"
+  },
+  {
+    "revision": "6c3dca680285e0bc545f",
+    "url": "/warehouses-ui-test/static/js/runtime-main.282f9d51.js"
+  }
+]);
